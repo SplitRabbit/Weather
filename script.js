52a2b2c3e452ee97164abf0d9117cfcf37221942
SplitRabbit <jue.kun.liu@gmail.com>
2022-04-10 10:47:50 -0400

    changed deployed application

--- script.js
+++ script.js
@@ -19,24 +19,42 @@ function addsearchhistory() {
     let searchhistoryentryEl = document.createElement("li")
     searchhistoryentryEl.textContent = cityInputEl.value
     //add classes here 
-    searchhistoryentryEl.classList = "row"
+    searchhistoryentryEl.classList = "row searchhistoryentry"
+    searchhistoryentryEl.addEventListener("click",clicksearchhistory)
     searchhistorylistEl.appendChild(searchhistoryentryEl)
 };
 
 //listen for clicks in the search history list
-searchhistorylistEl.addEventListener("click",clicksearchhistory);
-function clicksearchhistory() {
-    console.log()
+function clicksearchhistory(e) {
+    var li = e.currentTarget;
+    cityInputEl.value = li.textContent
+    console.log('potato')
     getweather(cityInputEl.value)
 };
+
+//increment date
+Date.prototype.addDays = function(days) {
+    var date = new Date(this.valueOf());
+    date.setDate(date.getDate() + days);
+    return date;
+}
+
 
 //fetch information from weather api
 function getweather(city) {
     let api_url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=6090a0c3c0ba8ad5f1c59b776c43013c'
-
+//use first api to find city lat and long
  fetch (api_url).then(function(response) {
      response.json().then(function(data) {
         console.log(data)
+            //grab lat and long 
+            let lat = data.coord.lat;
+            let lon = data.coord.lon;
+
+            //fetch api for 5 day forecast
+            fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+'&exclude=alerts,minutely,hourly&appid=6090a0c3c0ba8ad5f1c59b776c43013c').then(function(response) {
+                response.json().then(function(data) {
+                   console.log(data)
 
             //create searched city element
             var currentweatherEl = document.getElementById("currentweather");
@@ -44,9 +62,10 @@ function getweather(city) {
              currentweatherEl.innerHTML = "";
 
                 //declare weather variables
-                let temp = data.main.temp;
-                let wind = data.wind.speed;
-                let humidity = data.main.humidity;
+                let temp = data.current.temp;
+                let wind = data.current.wind_speed;
+                let humidity = data.current.humidity;
+                let uvi = data.current.uvi;
 
                 //grab temperature
                     let cityEl = document.createElement("h2")
@@ -58,44 +77,48 @@ function getweather(city) {
                 //grab temperature
                     let tempEl = document.createElement("div")
                     //add fetched information to card
-                    tempEl.textContent = "Temp" + ":" + temp
+                    tempEl.textContent = "Temp" + ": " + temp
                     currentweatherEl.appendChild(tempEl);
                     console.log(tempEl);
 
-                //grab temperature
+                //grab wind
                     let windEl = document.createElement("div")
                     //add fetched information to card
-                    windEl.textContent = "Wind" + ":" + wind
+                    windEl.textContent = "Wind" + ": " + wind
                     currentweatherEl.appendChild(windEl);
                     console.log(windEl);
 
-                //grab temperature
+                //grab humidity
                     let humidityEl = document.createElement("div")
                     //add fetched information to card
-                    humidityEl.textContent = "Humidity" + ":" + humidity
+                    humidityEl.textContent = "Humidity" + ": " + humidity
                     currentweatherEl.appendChild(humidityEl);
                     console.log(humidityEl);
 
-            //grab lat and long 
-            let lat = data.coord.lat;
-            let lon = data.coord.lon;
+                //grab uvi
+                    let uviEl = document.createElement("div")
+                    //add fetched information to card
+                    uviEl.textContent = "UVI" + ": " + uvi
+                    currentweatherEl.appendChild(uviEl);
+                    console.log(uviEl);
 
-            //fetch api for 5 day forecast
-            fetch('https://api.openweathermap.org/data/2.5/forecast?lat='+lat+'&lon='+lon+'&appid=6090a0c3c0ba8ad5f1c59b776c43013c').then(function(response) {
-                response.json().then(function(data) {
-                   console.log(data)
                    //loop through the next 5 days and append elements
                    var forecastweatherEl = document.getElementById("forecastweather")
                    forecastweatherEl.innerHTML = "";
                    numberdays = 5
                    for (let i = 0; i < numberdays; i++) {
                     //append elements
-                        let forecasttemp = data.list[i].main.temp;
-                        let forecastwind = data.list[i].wind.speed;
-                        let forecasthumidity = data.list[i].main.humidity;
+                        let forecasttemp = data.daily[i].temp.day;
+                        let forecastwind = data.daily[i].wind_speed;
+                        let forecasthumidity = data.daily[i].humidity;
+                        let forecastdate = (today.addDays(i+1)).toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"}) 
                     //append parent container
                         let forecastEl = document.createElement("div");
                         forecastEl.classList = "col container";
+                        //append current date
+                        let forecastdateEl = document.createElement("h5");
+                        forecastdateEl.textContent = forecastdate;
+                        forecastEl.appendChild(forecastdateEl);
                         //append temp
                         let forecasttempEl = document.createElement("div");
                         forecasttempEl.textContent = "Temp: " + forecasttemp
@@ -114,7 +137,9 @@ function getweather(city) {
                 });
             });
      });
-    });
+    }).catch(function(){
+        console.log('404 Not Found');
+      });
 };
 
 
